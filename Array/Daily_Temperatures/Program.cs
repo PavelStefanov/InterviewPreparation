763636b04a92e73964ac0f6ed22bce95f82bb220
Pavel Stefanov <pasha.i.stefanov@gmail.com>
2023-06-04 00:07:09 +0300

    Add more problems

---
+++ Array/Daily_Temperatures/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+var answer = new Solution().DailyTemperatures(new int[] { 34, 80, 80, 34, 34, 80, 80, 80, 80, 34 });
+Console.WriteLine("[{0}]", string.Join(", ", answer));
+
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var answers = new int[temperatures.Length];
+
+        for (var i = temperatures.Length - 2; i >= 0; i--)
+        {
+            if (temperatures[i] < temperatures[i + 1])
+            {
+                answers[i] = 1;
+            }
+            else if (temperatures[i] == temperatures[i + 1])
+            {
+                if (answers[i + 1] == 0)
+                {
+                    answers[i] = 0;
+                }
+                else
+                {
+                    answers[i] = answers[i + 1] + 1;
+                }
+            }
+            else
+            {
+                answers[i] = answers[i + 1];
+
+                var j = i + answers[i + 1] + 1;
+
+                if (temperatures[i] < temperatures[j])
+                {
+                    answers[i]++;
+                    continue;
+                }
+                if (temperatures[i] > temperatures[j] && answers[j] == 0)
+                {
+                    answers[i] = 0;
+                    continue;
+                }
+
+                for (; j < temperatures.Length;)
+                {
+                    if (temperatures[i] < temperatures[j])
+                    {
+                        answers[i]++;
+                        break;
+                    }
+                    else if (temperatures[i] >= temperatures[j] && answers[j] == 0)
+                    {
+                        answers[i] = 0;
+                        break;
+                    }
+                    else
+                    {
+                        answers[i] += answers[j];
+                        j += answers[j];
+                    }
+                }
+            }
+        }
+
+        return answers;
+    }
+}
---
+++ Tree/Sum_of_Nodes_with_Even-Valued_Grandparent/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+
+//   Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public int SumEvenGrandparent(TreeNode root)
+    {
+        return SumEvenGrandparent(root, false, false);
+    }
+
+    public int SumEvenGrandparent(TreeNode node, bool isGrandParentEven, bool isParentEven)
+    {
+        var result = 0;
+        if (node is null)
+        {
+            return result;
+        }
+
+        var isEven = node.val % 2 == 0;
+
+        if (isGrandParentEven)
+        {
+            result += node.val;
+        }
+        result += SumEvenGrandparent(node.left, isParentEven, isEven)
+             + SumEvenGrandparent(node.right, isParentEven, isEven);
+
+        return result;
+    }
+}
